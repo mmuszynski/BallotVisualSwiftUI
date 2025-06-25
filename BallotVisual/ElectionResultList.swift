abf77a56e17f7d659ecf63eea23d72d5c22f1482
Mike Muszynski <mmuszynski@gmail.com>
2025-06-25 15:26:59 -0400

    adding balloting results

---
+++ BallotVisual/ElectionResultList.swift
@@ -0,0 +1,18 @@
+//
+//  ElectionResultList.swift
+//  BallotVisual
+//
+//  Created by Mike Muszynski on 6/25/25.
+//
+
+import SwiftUI
+
+struct ElectionResultList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElectionResultList()
+}
